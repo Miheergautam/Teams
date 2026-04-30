ddfc622f5c7f1b71af1e4354186e7e839a8f2ff0
Miheer Gautam <miheergautam04@gmail.com>
2026-04-30 16:29:17 +0200

    feat: db setup with modular structure

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from shared.db.database import get_db
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.mongodb_client, app.database = get_db()
+    print("Connected to MongoDB")
+
+    yield
+
+    app.mongodb_client.close()
+    print("Disconnected from MongoDB")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
-def get_health_status():
+async def get_health_status():
     return {"status": "Teams ok"}
---
+++ backend/shared/db/database.py
@@ -0,0 +1,7 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import settings
+
+def get_db():
+    client = AsyncIOMotorClient(settings.mongo_url)
+    db = client[settings.db_name]
+    return client, db
