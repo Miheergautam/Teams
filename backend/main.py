7d25f16b1d3653520671216a0cf8119048327909
Miheer Gautam <miheergautam04@gmail.com>
2026-05-01 14:21:05 +0200

    cors origin fix

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://teams-production-4b58.up.railway.app", "http://127.0.0.1:8000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
